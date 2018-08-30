91dd259ba94f0cfaec48225520c543fc0458c4b5
Miaoshuyu <1941285191@qq.com>
2018-08-30 09:38:53 +0800

    the code of graph tutorial

---
+++ 02_graph_model.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# @Author  : Miaoshuyu
+# @Email   : miaohsuyu319@163.com
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from tensorboardX import SummaryWriter
+
+
+class Net1(nn.Module):
+    def __init__(self):
+        super(Net1, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+        self.bn = nn.BatchNorm2d(20)
+
+    def forward(self, x):
+        x = F.max_pool2d(self.conv1(x), 2)
+        x = F.relu(x) + F.relu(-x)
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = self.bn(x)
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        x = F.softmax(x, dim=1)
+        return x
+
+
+dummy_input = torch.rand(13, 1, 28, 28)
+
+model = Net1()
+with SummaryWriter(comment='Net1') as w:
+    w.add_graph(model, (dummy_input,))
